a2db15505dd87221bb5347620cd62c65600569b7
Gid3onJura <homebasekorlash@googlemail.com>
2024-04-02 01:00:22 +0200

    bugfix redirect

--- actions/login.ts
+++ actions/login.ts
@@ -7,37 +7,32 @@ import { DEFAULT_LOGIN_REDIRECT_URL } from "@/routes"
 import { AuthError } from "next-auth"
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
-  try {
-    // validate input values
-    const validatedFields = LoginSchema.safeParse(values)
+  // validate input values
+  const validatedFields = LoginSchema.safeParse(values)
 
-    if (!validatedFields.success) {
-      return { error: "Angaben falsch" }
-    }
-
-    const { nickname, password } = validatedFields.data
-
-    try {
-      await signIn("credentials", {
-        nickname,
-        password,
-        redirectTo: DEFAULT_LOGIN_REDIRECT_URL,
-      })
-    } catch (error) {
-      if (error instanceof AuthError) {
-        switch (error.type) {
-          case "CredentialsSignin":
-            return { error: "Überprüfe deine Eingaben!" }
-          default:
-            return { error: "Etwas lief schief!" }
-        }
-      }
-
-      throw error
-    }
-    return { error: "Etwas lief schief!" }
-  } catch (err) {
-    console.log(err)
-    return { error: "Login nicht erfolgreich" }
+  if (!validatedFields.success) {
+    return { error: "Angaben falsch" }
   }
+
+  const { nickname, password } = validatedFields.data
+
+  try {
+    await signIn("credentials", {
+      nickname,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT_URL,
+    })
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Überprüfe deine Eingaben!" }
+        default:
+          return { error: "Etwas lief schief!" }
+      }
+    }
+
+    throw error
+  }
+  return { error: "Etwas lief schief!" }
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,5 +1,19 @@
+import { signOut } from "@/auth"
+
 const HomePage = () => {
-  return <div>Homepage</div>
+  return (
+    <div>
+      <form
+        action={async () => {
+          "use server"
+
+          await signOut()
+        }}
+      >
+        <button type="submit">Ausloggen</button>
+      </form>
+    </div>
+  )
 }
 
 export default HomePage
