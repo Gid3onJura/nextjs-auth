167025b05124d32bbe7aea5a24bd26b4a29815be
Gid3onJura <homebasekorlash@googlemail.com>
2024-04-21 22:18:56 +0200

    refactoring

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -5,23 +5,7 @@ import { useCurrentUser } from "@/hooks/useCurrentUser"
 import { useSession, signOut } from "next-auth/react"
 
 const HomePage = () => {
-  const user = useCurrentUser()
-
-  const handleSignOut = () => {
-    // if you use on client side
-    // signOut()
-
-    // if you use on server side
-    logout()
-  }
-
-  return (
-    <div className="bg-white rounded-xl p-3">
-      <button onClick={handleSignOut} type="submit">
-        Ausloggen
-      </button>
-    </div>
-  )
+  return <div>Home</div>
 }
 
 export default HomePage
